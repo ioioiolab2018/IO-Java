3577e14de1ae0acdb4c856fa488ad99ddb47b066
pawelbubak <pawelbubak97@gmail.com>
2018-10-26 18:47:11 +0200

    initial commit

---
+++ src/main/java/pl/put/poznan/analyzer/logic/NetworkAnalyzer.java
@@ -0,0 +1,10 @@
+package pl.put.poznan.analyzer.logic;
+
+/**
+ * This is just an example to show that the logic should be outside the REST service.
+ */
+public class NetworkAnalyzer {
+
+    public NetworkAnalyzer(){
+    }
+}
