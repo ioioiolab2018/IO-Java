329ad0f36dfc00c91a912199b134342985034821
pawelbubak <pawelbubak97@gmail.com>
2018-11-04 14:26:49 +0100

    Poprawka konwerterów

--- src/main/java/pl/put/poznan/analyzer/converter/ConnectionConverter.java
+++ src/main/java/pl/put/poznan/analyzer/converter/ConnectionConverter.java
@@ -2,10 +2,12 @@ package pl.put.poznan.analyzer.converter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 import pl.put.poznan.analyzer.commons.Connection;
 
 import java.io.IOException;
 
+@Component
 public class ConnectionConverter implements Converter<String, Connection> {
     @Override
     public Connection convert(String s) {
