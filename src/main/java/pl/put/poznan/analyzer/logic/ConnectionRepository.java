bd9efd218250bbd269771e78bb0a09636c7883cb
Piotr <sensaj10@gmail.com>
2018-11-04 13:27:59 +0100

    Usunięcie pozostałości po JPA

--- src/main/java/pl/put/poznan/analyzer/logic/ConnectionRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.put.poznan.analyzer.logic;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.put.poznan.analyzer.commons.Connection;
-
-public interface ConnectionRepository extends CrudRepository<Connection, Long> {
-}
--- src/main/java/pl/put/poznan/analyzer/rest/NetworkAnalyzerController.java
+++ src/main/java/pl/put/poznan/analyzer/rest/NetworkAnalyzerController.java
@@ -2,11 +2,8 @@ package pl.put.poznan.analyzer.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.analyzer.commons.Node;
-import pl.put.poznan.analyzer.logic.ConnectionRepository;
-import pl.put.poznan.analyzer.logic.NodeRepository;
 
 import java.util.Arrays;
 
@@ -17,20 +14,13 @@ public class NetworkAnalyzerController {
 
     private static final Logger logger = LoggerFactory.getLogger(NetworkAnalyzerController.class);
 
-    @Autowired
-    private ConnectionRepository connectionRepository;
-
-    @Autowired
-    private NodeRepository nodeRepository;
-
-
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Node get(@PathVariable String text,
                     @RequestParam(value = "transforms", defaultValue = "upper,escape") String[] transforms) {
         // log the parameters
         logger.debug(text);
         logger.debug(Arrays.toString(transforms));
-        return nodeRepository.findOne((long) 1);
+        return null;
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
