620d8b2d7bb4b0e5a59f56a62922c57547971583
Stapior <sensaj10@gmail.com>
2018-11-06 18:11:26 +0100

    Poprawienie klasy Node

--- src/main/java/pl/put/poznan/analyzer/commons/Node.java
+++ src/main/java/pl/put/poznan/analyzer/commons/Node.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 public class Node {
 
-    private Integer id;
+    private int id;
     private String name;
     private NodeType nodeType;
     private List<Connection> outgoing;
@@ -33,11 +33,11 @@ public class Node {
         this.incoming = new ArrayList<>();
     }
 
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
